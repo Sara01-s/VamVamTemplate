b5e9a71e2455dfa4b0742e2c34a60285edadd5ec
Sara01 <saraosses01@gmail.com>
2023-08-03 14:20:19 -0400

    Added string extension to cut string between to chars

--- Assets/VVT/Source/Core/Extensions.cs
+++ Assets/VVT/Source/Core/Extensions.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+
 namespace VVT {
 
     internal static class Extensions {
@@ -11,7 +13,7 @@ namespace VVT {
         /// <returns>Cut string</returns>
         public static string CutAfter(this string value, char target, bool inclusive = true) {
             int index = inclusive ? value.IndexOf(target) : value.IndexOf(target) + 1;
-            return (index >= 0) ? value[.. index] : value;
+            return (index >= 0) ? value[.. index] : string.Empty;
         }
 
         /// <summary>
@@ -23,7 +25,29 @@ namespace VVT {
         /// <returns>Cut string</returns>
         public static string CutBefore(this string value, char target, bool exclusive = true) {
             int index = exclusive ? value.IndexOf(target) + 1 : value.IndexOf(target);
-            return (index >= 0) ? value[index ..] : value;
+            return (index >= 0) ? value[index ..] : string.Empty;
+        }
+
+        /// <summary>
+        /// Returns a cut string between two characters.
+        /// </summary>
+        /// <param name="value">The string to cut.</param>
+        /// <param name="start">Where the cut's starts.</param>
+        /// <param name="end">Where the cut's ends.</param>
+        /// <param name="inclusive">Should the cut include start and end?</param>
+        /// <returns>Cut string.</returns>
+        public static string CutBetween(this string value, char start, char end, bool inclusive = true) {
+            int startIndex = value.IndexOf(start);
+            int endIndex   = value.IndexOf(end);
+
+            if (startIndex >= 0 && endIndex > startIndex) {
+                int startIndexToUse = inclusive ? startIndex   : startIndex + 1;
+                int endIndexToUse   = inclusive ? endIndex + 1 : endIndex;
+
+                return value[startIndexToUse .. endIndexToUse];
+            }
+
+            return string.Empty;
         }
 
     }
