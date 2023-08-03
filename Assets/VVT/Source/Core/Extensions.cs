5e2c856fed79d641118f3ca944908774ddc0fa9d
Sara01 <saraosses01@gmail.com>
2023-08-03 14:05:08 -0400

    Added string extensions to cut after/before a character's string.

---
+++ Assets/VVT/Source/Core/Extensions.cs
@@ -0,0 +1,30 @@
+namespace VVT {
+
+    internal static class Extensions {
+
+        /// <summary>
+        /// Returns a cut string after the target char value.
+        /// </summary>
+        /// <param name="value">The string to cut.</param>
+        /// <param name="target">String will be cut after target character</param>
+        /// <param name="inclusive">Should the cut include the target character?</param>
+        /// <returns>Cut string</returns>
+        public static string CutAfter(this string value, char target, bool inclusive = true) {
+            int index = inclusive ? value.IndexOf(target) : value.IndexOf(target) + 1;
+            return (index >= 0) ? value[.. index] : value;
+        }
+
+        /// <summary>
+        /// Returns a cut string after the target char value.
+        /// </summary>
+        /// <param name="value">The string to cut.</param>
+        /// <param name="target">String will be cut after target character</param>
+        /// <param name="exclusive">Should the cut include the target character?</param>
+        /// <returns>Cut string</returns>
+        public static string CutBefore(this string value, char target, bool exclusive = true) {
+            int index = exclusive ? value.IndexOf(target) + 1 : value.IndexOf(target);
+            return (index >= 0) ? value[index ..] : value;
+        }
+
+    }
+}
