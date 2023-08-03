1a3d49b8f96c7aa219cc454e54ab36e158417e4a
Sara01 <saraosses01@gmail.com>
2023-08-03 14:07:39 -0400

    Extension CutBefore: Summary correction.

--- Assets/VVT/Source/Core/Extensions.cs
+++ Assets/VVT/Source/Core/Extensions.cs
@@ -15,11 +15,11 @@ namespace VVT {
         }
 
         /// <summary>
-        /// Returns a cut string after the target char value.
+        /// Returns a cut string before the target char value.
         /// </summary>
         /// <param name="value">The string to cut.</param>
-        /// <param name="target">String will be cut after target character</param>
-        /// <param name="exclusive">Should the cut include the target character?</param>
+        /// <param name="target">String will be cut before target character</param>
+        /// <param name="exclusive">Should the cut exclude the target character?</param>
         /// <returns>Cut string</returns>
         public static string CutBefore(this string value, char target, bool exclusive = true) {
             int index = exclusive ? value.IndexOf(target) + 1 : value.IndexOf(target);
