2619d5fa1b0c28cabcf18bfe447a9c9d294ab0e3
Sara01 <saraosses01@gmail.com>
2023-09-11 10:41:17 -0300

    Documented Mixer enum, added ErrorCode enum

--- Assets/VVT/Source/Core/Enums.cs
+++ Assets/VVT/Source/Core/Enums.cs
@@ -1,4 +1,23 @@
 namespace VVT {
     /// <summary> Audio mixers buses </summary>
-    public enum Mixer { Master, Music, Ambience, SFX }
+    public enum Mixer { 
+        /// <summary> Every sound final output bus </summary>
+        Master, 
+        /// <summary> Mixer bus for playing music (e.g. songs, instrumentals, game OST)</summary>
+        Music, 
+        /// <summary> Mixer bus for playing ambience sounds (e.g. wind) </summary>
+        Ambience, 
+        /// <summary> Mixer bus for playing sound effects (e.g. jump, shoot, footsteps) </summary>
+        SFX 
+    }
+
+    /// <summary> Error codes for identifying underlying VVT problems 
+    public enum ErrorCode { 
+        /// <summary> An unkown generic error happened </summary>
+        GenericError = 0,
+        /// <summary> An unexpected argument was pass to the function </summary>
+        BadArgument = 1,
+        /// <summary> Can't get queried resource, the resource is unreachable or null </summary>
+        InvalidResource = 2,
+    }
 }
