78b2bbce987b6405b1b30ac60f58ed449e716069
Sara01 <saraosses01@gmail.com>
2024-01-29 22:51:01 -0300

    added Timer documentation

--- Assets/VVT/Source/Core/Timer.cs
+++ Assets/VVT/Source/Core/Timer.cs
@@ -2,15 +2,32 @@ using System;
 
 namespace VVT {
 
+	/// <summary>
+    /// Represents a timer that triggers an event when it ends.
+    /// </summary>
     public sealed class Timer {
 
+		/// <summary>
+        /// Gets the remaining seconds on the timer.
+        /// </summary>
         public float RemainingSeconds { get; private set; }
+		/// <summary>
+        /// Occurs when the timer ends.
+        /// </summary>
         public event Action OnTimerEnd;
 
-        public Timer(float duration) {
-            RemainingSeconds = duration;
+		/// <summary>
+        /// Creates a new timer with the specified duration.
+        /// </summary>
+        /// <param name="durationSeconds">The duration of the timer in seconds.</param>
+        public Timer(float durationSeconds) {
+            RemainingSeconds = durationSeconds;
         }
 
+		/// <summary>
+        /// Decreases the remaining seconds on the timer by the specified amount.
+        /// </summary>
+        /// <param name="deltaTime">The amount of time to decrease from the timer.</param>
         public void Tick(float deltaTime) {
             if (RemainingSeconds <= 0) return;
 
@@ -21,6 +38,7 @@ namespace VVT {
 
         private void CheckForTimerEnd() {
             if (RemainingSeconds > 0f) return;
+
             RemainingSeconds = 0f;
             OnTimerEnd?.Invoke();
         }
